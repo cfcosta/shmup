8393b5962a5a10e1873cb99e038b7ebe13b6c4a1
Cainã Costa <cainan.costa@gmail.com>
2011-04-22 02:33:45 -0300

    Added framerate control on the main() function

--- src/main.c
+++ src/main.c
@@ -4,6 +4,13 @@
 
 int main (int argc, char ** argv)
 {
+    const int frames_per_second = 60;
+    const int skip_frames = 1000 / frames_per_second;
+    const int max_frameskip = 10;
+
+    Uint32 next_game_frame = SDL_GetTick();
+    int loops = 0;
+
     if (!create_display()) {
       printf("Could not initialize the window.\n");
       printf("%s", SDL_GetError());
@@ -13,6 +20,12 @@ int main (int argc, char ** argv)
     SDL_WM_SetCaption("SDL Shmup", "SDL Shmup");
 
     while(1) {
+        while (SDL_GetTick() > next_game_frame && loops > max_frameskip) {
+
+          next_game_frame += skip_frames;
+          loops++;
+        }
+
         update_display();
     }
 
