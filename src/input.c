9a6bd30140cecae2e4029e85fd19b59cd420f1cd
Cainã Costa <cainan.costa@gmail.com>
2011-04-22 08:08:58 -0300

    Added a simple direction handling.

--- src/input.c
+++ src/input.c
@@ -15,8 +15,31 @@ void update_input()
             case SDL_KEYDOWN:
                 handle_keypress(event.key.keysym.sym);
                 break;
+            case SDL_KEYUP:
+                handle_keyup(event.key.keysym.sym);
+                break;
         }
     }
+
+    if (!pressed_up) speed_up--;
+    if (!pressed_down) speed_down--;
+    if (!pressed_left) speed_left--;
+    if (!pressed_right) speed_right--;
+
+    if (speed_up > 3) speed_up = 3;
+    if (speed_down > 3) speed_down = 3;
+    if (speed_left > 3) speed_left = 3;
+    if (speed_right > 3) speed_right = 3;
+
+    if (speed_up < 0) speed_up = 0;
+    if (speed_down < 0) speed_down = 0;
+    if (speed_left < 0) speed_left = 0;
+    if (speed_right < 0) speed_right = 0;
+
+    ship_rect.y += speed_up;
+    ship_rect.y -= speed_down;
+    ship_rect.x += speed_right;
+    ship_rect.x -= speed_left;
 }
 
 void handle_keypress(SDLKey key)
@@ -25,6 +48,38 @@ void handle_keypress(SDLKey key)
         case SDLK_ESCAPE:
             quit = 1;
             break;
+        case SDLK_UP:
+            pressed_up = 1;
+            speed_up++;
+            speed_down--;
+        case SDLK_DOWN:
+            pressed_down = 1;
+            speed_down++;
+            speed_up--;
+        case SDLK_LEFT:
+            pressed_left = 1;
+            speed_left++;
+            speed_right--;
+        case SDLK_RIGHT:
+            pressed_right = 1;
+            speed_right++;
+            speed_left--;
+        default:
+            break;
+    }
+}
+
+void handle_keyup(SDLKey key)
+{
+    switch(key) {
+        case SDLK_UP:
+            pressed_up = 0;
+        case SDLK_DOWN:
+            pressed_down = 0;
+        case SDLK_LEFT:
+            pressed_left = 0;
+        case SDLK_RIGHT:
+            pressed_right = 0;
         default:
             break;
     }
--- src/input.h
+++ src/input.h
@@ -1,10 +1,22 @@
 #include "common.h"
+#include "graphics.h"
 
 #include <signal.h>
 
 int quit;
 void catch_signal(int signal);
 
+int speed_up;
+int speed_down;
+int speed_left;
+int speed_right;
+
+int pressed_up;
+int pressed_down;
+int pressed_left;
+int pressed_right;
+
 SDL_Event event;
 void update_input();
 void handle_keypress(SDLKey key);
+void handle_keyup(SDLKey key);
